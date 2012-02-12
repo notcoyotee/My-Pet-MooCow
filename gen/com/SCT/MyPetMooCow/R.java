5373445b3fbcbbfe826a9b3ce5b104b45e8ba90b
Gbzy Gil <gbzygil@gmail.com>
2012-02-12 10:12:43 -0500

    Revised

--- gen/com/SCT/MyPetMooCow/R.java
+++ gen/com/SCT/MyPetMooCow/R.java
@@ -11,8 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int happycow=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int cowicon=0x7f020000;
+        public static final int happycow=0x7f020001;
+        public static final int happycow2=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int button_play=0x7f060000;
--- src/com/SCT/MyPetMooCow/MyPetMooCowActivity.java
+++ src/com/SCT/MyPetMooCow/MyPetMooCowActivity.java
@@ -2,35 +2,67 @@ package com.SCT.MyPetMooCow;
 
 import java.util.Random;
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Color;
 
 public class MyPetMooCowActivity extends Activity {
-	
 	public String[] filelist;
+	AssetFileDescriptor descriptor;
+    private int mfile[] = new int[12];
+    private MediaPlayer mp;
+    public static Integer counter=1;
+    
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
-		SoundManager.getInstance();
-		SoundManager.initSounds(this);
-		SoundManager.loadSounds();
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+        mfile[0] = R.raw.a1;  
+        mfile[1] = R.raw.a2;  
+        mfile[2] = R.raw.a3;
+        mfile[3] = R.raw.a4;  
+        mfile[4] = R.raw.a5;  
+        mfile[5] = R.raw.a6;
+        mfile[6] = R.raw.a7;  
+        mfile[7] = R.raw.a8;  
+        mfile[8] = R.raw.a9;  
+        mfile[9] = R.raw.a10;  
+        mfile[10] = R.raw.a11;  
+        mfile[11] = R.raw.a12;       
+		final Random randomGenerator = new Random();
 
-		ImageButton playSound = (ImageButton)findViewById(R.id.button_play);
+		
+		final ImageButton playSound = (ImageButton) findViewById(R.id.button_play);
+		playSound.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				counter=counter+1;
+				Log.d("GBC", "counter =" + counter);
 
-		playSound.setOnClickListener(new View.OnClickListener() 
-		{           
-			public void onClick(View v) 
-			{
-				//SoundManager.stopSound(index)
-				Random randomGenerator = new Random();
-				int randomInt = randomGenerator.nextInt(12);
-				SoundManager.playSound(randomInt, 1);
+				if (counter%2 ==0) {
+				playSound.setImageResource(R.drawable.happycow2);
+				}
+				else
+				{
+					playSound.setImageResource(R.drawable.happycow);
+					
+				}
+				//playSound.setColorFilter(Color.argb(255, 255, 255, 255));
+				int randomInt = randomGenerator.nextInt(11);
+				Log.d("GBC", "Random =" + randomInt);
+				if (randomInt > 11) randomInt=2;
+	             mp = MediaPlayer.create(MyPetMooCowActivity.this, mfile[randomInt]);
+	             mp.seekTo(0);
+	             mp.start();
+				//playSound.setImageResource(R.drawable.happycow);
+
 			}
-		});   
+		});
+
 	}
 }
